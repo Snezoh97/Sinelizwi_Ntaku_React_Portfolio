d76d4c825e2548d337ca3adbe26c6945443e5277
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-21 14:28:31 +0200

    Added Home to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import NavBar from './components/NavBar.jsx'
+import Home from './components/Home'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
-    
+    <Home />
   </React.StrictMode>,
 )
