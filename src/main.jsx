9dc1c537f433b614f959b71bc85f01854306e86b
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-22 15:32:21 +0200

    Editing about section

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import NavBar from './components/NavBar.jsx'
+import NavBar from './components/NavBar'
 import Home from './components/Home'
-import About from './components/About'
+import AboutSection from './components/About'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar />
-    <Home />
-    <About />
+   <NavBar />
+   <Home />
+   <AboutSection />
   </React.StrictMode>,
 )
