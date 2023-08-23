c4050fe4069b6b0096e905154cf1cbd9265c1f72
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-23 10:17:57 +0200

    editing portfolio sections

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Portfolio = () => {
+function Portfolio (){
   return (
     <section className="portfolio" id="portfolio">
       <h2 className="heading">Latest <span>Projects</span></h2>
       <div className="portfolio-container">
         <div className="portfolio-box">
-          <img src="https://i.ibb.co/qkjfyYX/fcalc.png" alt="" />
+          <img src="https://i.ibb.co/qkjfyYX/fcalc.png" alt="fcal" />
           <div className="portfolio-layer">
             <h4>Finance Calculator</h4>
             <p>I have developed a Python-based finance calculator available on my GitHub. 
@@ -16,7 +16,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="portfolio-box">
-          <img src="images/nexus.png" alt="" />
+          <img src="https://i.ibb.co/8mj1PWv/nexus.png" alt="nexus"/>
           <div className="portfolio-layer">
             <h4>Financial Banking App</h4>
             <p>I have a Python-based financial banking app, available on my GitHub profile. 
@@ -26,7 +26,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="portfolio-box">
-          <img src="images/bank_app.png" alt="" />
+          <img src="https://i.ibb.co/RvGMq7Z/bank-app.png" alt="bank-app" />
           <div className="portfolio-layer">
             <h4>Nexus Banking App</h4>
             <p>Python-based banking app on my GitHub profile for seamless financial transactions, including transfers, balance inquiries, and transaction history. 
@@ -36,7 +36,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="portfolio-box">
-          <img src="images/school.png" alt="" />
+          <img src="https://i.ibb.co/HqpJnW2/school.png" alt="school" />
           <div className="portfolio-layer">
             <h4>School Website</h4>
             <p>I have developed a school website using HTML, CSS, and JavaScript, available on my GitHub. 
@@ -46,7 +46,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="portfolio-box">
-          <img src="images/unit_app.png" alt="" />
+          <img src="https://i.ibb.co/2Y6scdT/unit-app.png" alt="unit-app" />
           <div className="portfolio-layer">
             <h4>Unit Converter</h4>
             <p>Explore my Java-based unit converter on GitHub, a versatile tool for converting various measurements and quantities. 
@@ -56,7 +56,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="portfolio-box">
-          <img src="images/game.png" alt="" />
+          <img src="https://i.ibb.co/Twxy2Xj/game.png" alt="game"/>
           <div className="portfolio-layer">
             <h4>Mzamomtsha Games</h4>
             <p>Embark on exciting adventures with a collection of captivating games developed using HTML, CSS, and JavaScript. 
