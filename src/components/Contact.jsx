5af27f923841a0b68c00746f7061399f1a409800
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-24 09:37:40 +0200

    Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,82 +2,38 @@ import React from 'react';
 
 function Contact(){
   return (
-    <section
-      id="contact"
-      className="paralax-mf footer-paralax bg-image sect-mt4 route"
-      style={{ backgroundImage: "url(assets/img/overlay-bg.jpg)" }}
-    >
-      <div className="overlay-mf"></div>
+    <section id="contact" className="contact">
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <div className="contact-mf">
-              <div id="contact" className="box-shadow-full">
+            <div className="contact-sec">
+              <div className="box-shadow-full">
                 <div className="row">
                   <div className="col-md-6">
                     <div className="title-box-2">
                       <h5 className="title-left">Send Message Us</h5>
                     </div>
                     <div>
-                      <form
-                        action="forms/contact.php"
-                        method="post"
-                        role="form"
-                        className="php-email-form"
-                      >
+                      <form action="" method="" role="form" className="email-form">
                         <div className="row">
                           <div className="col-md-12 mb-3">
                             <div className="form-group">
-                              <input
-                                type="text"
-                                name="name"
-                                className="form-control"
-                                id="name"
-                                placeholder="Your Name"
-                                required
-                              />
+                              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required  />
                             </div>
                           </div>
                           <div className="col-md-12 mb-3">
                             <div className="form-group">
-                              <input
-                                type="email"
-                                className="form-control"
-                                name="email"
-                                id="email"
-                                placeholder="Your Email"
-                                required
-                              />
+                              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                             </div>
                           </div>
                           <div className="col-md-12 mb-3">
                             <div className="form-group">
-                              <input
-                                type="text"
-                                className="form-control"
-                                name="subject"
-                                id="subject"
-                                placeholder="Subject"
-                                required
-                              />
+                              <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
                             </div>
                           </div>
                           <div className="col-md-12">
                             <div className="form-group">
-                              <textarea
-                                className="form-control"
-                                name="message"
-                                rows="5"
-                                placeholder="Message"
-                                required
-                              ></textarea>
-                            </div>
-                          </div>
-                          <div className="col-md-12 text-center my-3">
-                            <div className="loading">Loading</div>
-                            <div className="error-message"></div>
-                            <div className="sent-message">
-                              Your message has been sent. Thank you!
+                              <textarea className="form-control" name="message" rows="5" placeholder="Message" required ></textarea>
                             </div>
                           </div>
                           <div className="col-md-12 text-center">
