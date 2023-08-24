61e1b6d55643eb768237979f086ac6d23a65bd43
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-24 09:39:02 +0200

    Contact coding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,119 +1,67 @@
 import React from 'react';
 
-function Contact(){
+const ContactForm = () => {
   return (
-    <section id="contact" className="contact">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="contact-sec">
-              <div className="box-shadow-full">
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="title-box-2">
-                      <h5 className="title-left">Send Message Us</h5>
-                    </div>
-                    <div>
-                      <form action="" method="" role="form" className="email-form">
-                        <div className="row">
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required  />
-                            </div>
-                          </div>
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
-                            </div>
-                          </div>
-                          <div className="col-md-12 mb-3">
-                            <div className="form-group">
-                              <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
-                            </div>
-                          </div>
-                          <div className="col-md-12">
-                            <div className="form-group">
-                              <textarea className="form-control" name="message" rows="5" placeholder="Message" required ></textarea>
-                            </div>
-                          </div>
-                          <div className="col-md-12 text-center">
-                            <button
-                              type="submit"
-                              className="button button-a button-big button-rouded"
-                            >
-                              Send Message
-                            </button>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="title-box-2 pt-4 pt-md-0">
-                      <h5 className="title-left">Get in Touch</h5>
-                    </div>
-                    <div className="more-info">
-                      <p className="lead">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis dolorum dolorem soluta quidem expedita aperiam
-                        aliquid at. Totam magni ipsum suscipit amet? Autem nemo
-                        esse laboriosam ratione nobis mollitia inventore?
-                      </p>
-                      <ul className="list-ico">
-                        <li>
-                          <span className="bi bi-geo-alt"></span> 329
-                          WASHINGTON ST BOSTON, MA 02108
-                        </li>
-                        <li>
-                          <span className="bi bi-phone"></span> (617) 557-0089
-                        </li>
-                        <li>
-                          <span className="bi bi-envelope"></span>{" "}
-                          contact@example.com
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="socials">
-                      <ul>
-                        <li>
-                          <a href="">
-                            <span className="ico-circle">
-                              <i className="bi bi-facebook"></i>
-                            </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="">
-                            <span className="ico-circle">
-                              <i className="bi bi-instagram"></i>
-                            </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="">
-                            <span className="ico-circle">
-                              <i className="bi bi-twitter"></i>
-                            </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="">
-                            <span className="ico-circle">
-                              <i className="bi bi-linkedin"></i>
-                            </span>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          {/* First code block */}
+          <div className="title-box-2 pt-4 pt-md-0">
+            <h5 className="title-left">Get in Touch</h5>
+          </div>
+          <div className="more-info">
+            <p className="lead">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Facilis dolorum dolorem soluta quidem expedita aperiam
+              aliquid at. Totam magni ipsum suscipit amet? Autem nemo
+              esse laboriosam ratione nobis mollitia inventore?
+            </p>
+            <ul className="list-ico">
+              <li>
+                <span className="bi bi-geo-alt"></span> 329 WASHINGTON ST BOSTON, MA 02108
+              </li>
+              <li>
+                <span className="bi bi-phone"></span> (617) 557-0089
+              </li>
+              <li>
+                <span className="bi bi-envelope"></span> contact@example.com
+              </li>
+            </ul>
+          </div>
+          <div className="socials">
+            <ul>
+              <li>
+                <a href="">
+                  <span className="ico-circle">
+                    <i className="bi bi-facebook"></i>
+                  </span>
+                </a>
+              </li>
+              {/* Add other social media links here */}
+            </ul>
           </div>
         </div>
+        <div className="col-md-6">
+          {/* Second code block */}
+          <section className="contact" id="contact">
+            <h2 className="heading">Contact <span>Me!</span></h2>
+            <form>
+              <div className="input-box">
+                <input required type="text" name="text" placeholder="Full Name" autoComplete="off" id="name" />
+                <input type="email" placeholder="Email Address" autoComplete="off" id="email" />
+              </div>
+              <div className="input-box">
+                <input type="number" placeholder="Mobile Number" autoComplete="off" id="mobile" />
+                <input type="text" placeholder="Email Subject" autoComplete="off" id="subject" />
+              </div>
+              <textarea name="" id="message" cols="30" rows="10" placeholder="Your Message" autoComplete="off"></textarea>
+              <input type="button" onClick={SendEmail} value="Send Message" className="btn" />
+            </form>
+          </section>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
-export default Contact;
+export default ContactForm;
