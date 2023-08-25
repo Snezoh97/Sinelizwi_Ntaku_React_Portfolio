d67a19abc73f8ba48c56aa96d47e60c1ca930961
Snezoh97 <sinelizwitaku@gmail.com>
2023-08-25 10:42:23 +0200

    portfolio box

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,57 +12,54 @@ function Portfolio (){
             <p className='boxParagraph'>I have developed a Python-based finance calculator available on my GitHub. 
               Feel free to explore the project on my GitHub for the code and details. 
             </p>
-            <a href="https://github.com/Snezoh97/Financial_Calculator" title="Financial Calculator on GitHub"><i id='link' className='bx bx-link-external' ></i></a>
+            <a href="https://github.com/Snezoh97/Financial_Calculator" title="Financial Calculator on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
         <div className="portfolio-box">
           <img src="https://i.ibb.co/8mj1PWv/nexus.png" alt="nexus"/>
           <div className="portfolio-layer">
-            <h4 className='boxHeading'>Financial Banking App</h4>
+            <h4 className='boxHeading'>Banking App</h4>
             <p className='boxParagraph'>I have a Python-based financial banking app, available on my GitHub profile. 
-              It provides secure banking features, and intuitive user interfaces for a streamlined banking experience. 
+              It provides secure banking features, and intuitive user interfaces. 
             </p>
-            <a href="https://i.ibb.co/RvGMq7Z/bank-app.png" title="Financial Banking App on GitHub"><i id='link' className='bx bx-link-external' ></i></a>
+            <a href="https://i.ibb.co/RvGMq7Z/bank-app.png" title="Financial Banking App on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
         <div className="portfolio-box">
           <img src="https://i.ibb.co/RvGMq7Z/bank-app.png" alt="bank-app" />
           <div className="portfolio-layer">
-            <h4>Nexus Banking App</h4>
-            <p>Python-based banking app on my GitHub profile for seamless financial transactions, including transfers, balance inquiries, and transaction history. 
-              Experience its power by exploring the code and functionalities. 
+            <h4 className='boxHeading'>Nexus App</h4>
+            <p className='boxParagraph'>Python-based banking app on my GitHub profile for seamless financial transactions, including transfers, balance inquiries, and transaction history. 
             </p>
-            <a href="https://github.com/Snezoh97/Bank_App" title="Bank App on GitHub"><i className='bx bx-link-external' ></i></a>
+            <a href="https://github.com/Snezoh97/Bank_App" title="Bank App on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
         <div className="portfolio-box">
           <img src="https://i.ibb.co/HqpJnW2/school.png" alt="school" />
           <div className="portfolio-layer">
-            <h4>School Website</h4>
-            <p>I have developed a school website using HTML, CSS, and JavaScript, available on my GitHub. 
-              It offers essential information, and a visually appealing design for an engaging user experience. 
+            <h4 className='boxHeading'>School Website</h4>
+            <p className='boxParagraph'>I have developed a school website using HTML, CSS, and JavaScript, available on my GitHub. 
+              It offers essential information. 
             </p>
-            <a href="https://github.com/Snezoh97/Mzamomtsha_Website" title="School website on GitHub"><i className='bx bx-link-external' ></i></a>
+            <a href="https://github.com/Snezoh97/Mzamomtsha_Website" title="School website on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
         <div className="portfolio-box">
           <img src="https://i.ibb.co/2Y6scdT/unit-app.png" alt="unit-app" />
           <div className="portfolio-layer">
-            <h4>Unit Converter</h4>
-            <p>Explore my Java-based unit converter on GitHub, a versatile tool for converting various measurements and quantities. 
-              Simplify conversions and enhance productivity with this open-source project. 
+            <h4 className='boxHeading'>Unit Converter</h4>
+            <p className='boxParagraph'>Explore my Java-based unit converter on GitHub, a versatile tool for converting various measurements and quantities. 
             </p>
-            <a href="https://github.com/Snezoh97/Unit_Converter" title="Unit Converter on GitHub"><i className='bx bx-link-external' ></i></a>
+            <a href="https://github.com/Snezoh97/Unit_Converter" title="Unit Converter on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
         <div className="portfolio-box">
           <img src="https://i.ibb.co/Twxy2Xj/game.png" alt="game"/>
           <div className="portfolio-layer">
-            <h4>Mzamomtsha Games</h4>
-            <p>Embark on exciting adventures with a collection of captivating games developed using HTML, CSS, and JavaScript. 
-              Explore my GitHub profile to discover the immersive gameplay and innovative designs. 
+            <h4 className='boxHeading'>School Games</h4>
+            <p className='boxParagraph'>Embark on exciting adventures with a collection of captivating games developed using HTML, CSS, and JavaScript. 
             </p>
-            <a href="https://github.com/Snezoh97/Games_Mzamomtsha" title="Games on GitHub"><i className='bx bx-link-external' ></i></a>
+            <a href="https://github.com/Snezoh97/Games_Mzamomtsha" title="Games on GitHub"><i className='bx bx-link-external link' ></i></a>
           </div>
         </div>
       </div>
