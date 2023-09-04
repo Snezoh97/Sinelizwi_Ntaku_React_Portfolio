6ad479f6694b9a0e6faeb3a6b79f9a44f4f7a512
unknown <Snezoh97>
2023-09-04 14:39:46 +0200

    tetstimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,65 +5,69 @@ function Testimonials() {
   const [isSliding, setIsSliding] = useState(false);
 
   useEffect(() => {
-    // Automatically add the animation class after a delay (e.g., 2 seconds)
+    
     const delay = setTimeout(() => {
       setIsSliding(true);
-    }, 2000); // Adjust the delay as needed (in milliseconds)
+    }, 3000); 
 
-    // Clean up the timeout when the component unmounts
+
     return () => clearTimeout(delay);
   }, []);
+
+  const scrollLeft = () => {
+    // Scroll the testimonials to the left
+    const testimonialContainer = document.querySelector('.testimonial-container');
+    if (testimonialContainer) {
+      testimonialContainer.scrollLeft -= 300; // Adjust the scroll distance as needed
+    }
+  };
 
   return (
     <section className="testimonials" id="testimonials">
       <h2>Testimo<span>nials</span></h2>
       <div className="border"></div>
-      <div className={`row-1 ${isSliding ? 'slide-left' : ''}`}>
-        <div className="col">
-          <div className="testimonial card">
-            <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
-            <div className="name">Sinelizwi Ntaku</div>
-            <div className="media-icons">
-                  <a href="" title="Facebook"><i className='bx bxl-facebook'></i></a>
-                  <a href="" title="Twitter"><i className='bx bxl-twitter'></i></a>
-                  <a href="" title="Instagram"><i className='bx bxl-instagram' ></i></a>
-                </div>
-                <p className="profession"><i className='bx bxs-quote-left qoutes'></i> Explore my Java-based unit converter 
-                on GitHub, a versatile tool for converting various measurements and quantities.<i className='bx bxs-quote-right qoutes'></i></p>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="testimonial card">
-            <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
-            <div className="name">Sinelizwi Ntaku</div>
-            <div className="media-icons">
-                  <a href="" title="Facebook"><i className='bx bxl-facebook'></i></a>
-                  <a href="" title="Twitter"><i className='bx bxl-twitter'></i></a>
-                  <a href="" title="Instagram"><i className='bx bxl-instagram' ></i></a>
-                </div>
-                <p className="profession"><i className='bx bxs-quote-left qoutes'></i> Explore my Java-based unit converter 
-                on GitHub, a versatile tool for converting various measurements and quantities.<i className='bx bxs-quote-right qoutes'></i></p>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="testimonial card">
-            <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
-            <div className="name">Sinelizwi Ntaku</div>
-            <div className="media-icons">
-                  <a href="" title="Facebook"><i className='bx bxl-facebook'></i></a>
-                  <a href="" title="Twitter"><i className='bx bxl-twitter'></i></a>
-                  <a href="" title="Instagram"><i className='bx bxl-instagram' ></i></a>
-                </div>
-                <p className="profession"><i className='bx bxs-quote-left qoutes'></i> Explore my Java-based unit converter 
-                on GitHub, a versatile tool for converting various measurements and quantities.<i className='bx bxs-quote-right qoutes'></i></p>
-          </div>
-        </div>
-
+      <div className={` row-1 testimonial-container ${isSliding ? 'slide-left' : ''}`}>
+        {window.innerWidth >= 1024 ? (
+          <>
+            <TestimonialSlide />
+            <TestimonialSlide />
+            <TestimonialSlide />
+         
+          </>
+        ) : window.innerWidth >= 768 ? (
+        
+          <>
+            <TestimonialSlide />
+            <TestimonialSlide />
+          </>
+        ) : (
+       
+          <TestimonialSlide />
+        )}
       </div>
     </section>
   );
 }
 
+function TestimonialSlide() {
+  return (
+    <div className="col">
+      <div className="testimonial card">
+        <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
+        <div className="name">Sinelizwi Ntaku</div>
+        <div className="media-icons">
+          <a href="" title="Facebook"><i className='bx bxl-facebook'></i></a>
+          <a href="" title="Twitter"><i className='bx bxl-twitter'></i></a>
+          <a href="" title="Instagram"><i className='bx bxl-instagram' ></i></a>
+        </div>
+        <p className="profession"><i className='bx bxs-quote-left qoutes'></i> Explore my Java-based unit converter
+          on GitHub, a versatile tool for converting various measurements and quantities.<i className='bx bxs-quote-right qoutes'></i></p>
+      </div>
+    </div>
+  );
+}
+
 export default Testimonials;
+
+
+
