88e74ca9ad8adde23724ee58f365bd9f84c26793
unknown <Snezoh97>
2023-09-15 11:01:23 +0200

    'responsiveness'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './Footer.css'
-import 'boxicons/css/boxicons.min.css';
+import React from "react";
+import "./Footer.css";
+import "boxicons/css/boxicons.min.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-text">
-                <p>Copyright &copy; 2023 Developed By Sinelizwi Ntaku | All Right Reserved.</p>
-            </div>
-            <div className="footer-iconTop">
-                <a href="#home" title="Top"><i className='bx bx-up-arrow-alt'></i>
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-text">
+        <p>
+          Copyright &copy; 2023 Developed By Sinelizwi Ntaku | All Right
+          Reserved.
+        </p>
+      </div>
+      <div className="footer-iconTop">
+        <a href="#home" title="Top">
+          <i className="bx bx-up-arrow-alt"></i>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
