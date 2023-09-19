67ecb93ea7c85b81528359e1a42b11dbf7108630
unknown <Snezoh97>
2023-09-19 13:55:20 +0200

    Responsiveness

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -111,5 +111,4 @@ function Portfolio() {
     </section>
   );
 }
-
 export default Portfolio;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -49,7 +49,7 @@ function Testimonials() {
           <div className="testmonials-slider">
             <div className="testimonial-card">
               <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
-              <div className="name">1</div>
+              <div className="name">Dr. S Kasana</div>
               <div className="media-icons">
                 <a href="" title="Facebook">
                   <i className="bx bxl-facebook"></i>
@@ -72,7 +72,31 @@ function Testimonials() {
           <div className="testmonials-slider">
             <div className="testimonial-card">
               <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
-              <div className="name">2</div>
+              <div className="name">Siyamthanda</div>
+              <div className="media-icons">
+                <a href="" title="Facebook">
+                  <i className="bx bxl-facebook"></i>
+                </a>
+                <a href="" title="Twitter">
+                  <i className="bx bxl-twitter"></i>
+                </a>
+                <a href="" title="Instagram">
+                  <i className="bx bxl-instagram"></i>
+                </a>
+              </div>
+              <p className="profession">
+                <i className="bx bxs-quote-left quotes"></i> Explore my
+                Java-based unit converter on GitHub, a versatile tool for
+                converting various measurements and quantities.{" "}
+                <i className="bx bxs-quote-right quotes"></i>
+              </p>
+            </div>
+          </div>
+
+          <div className="testmonials-slider">
+            <div className="testimonial-card">
+              <img src="https://i.ibb.co/JcT27q8/about-img.png" alt="" />
+              <div className="name">Sinethemba</div>
               <div className="media-icons">
                 <a href="" title="Facebook">
                   <i className="bx bxl-facebook"></i>
