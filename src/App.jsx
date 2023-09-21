eee4e7bf3461a1e91308ee225b470a41e1ab4a2b
unknown <Snezoh97>
2023-09-21 08:48:43 +0200

    testimnonials

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home'
 import NavBar from './components/NavBar'
 import Portfolio from './components/Portfolio'
 import Services from './components/Services'
-import Testimonials from './components/Testimonials'
+
 
 function App() {
 
@@ -19,7 +19,6 @@ function App() {
         <About/>
         <Services/>
         <Portfolio/>
-        <Testimonials/>
         <Contact/>
         <Footer/>
       </div>
