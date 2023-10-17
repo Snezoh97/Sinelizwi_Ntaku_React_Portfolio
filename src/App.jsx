ff60323c147d13f6a6ea90551c57529061a9f34b
unknown <Snezoh97>
2023-10-17 09:21:14 +0200

    navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home'
 import NavBar from './components/NavBar'
 import Portfolio from './components/Portfolio'
 import Services from './components/Services'
-import Testimonials from './components/Testimonials'
+
 
 
 function App() {
@@ -20,7 +20,6 @@ function App() {
         <About/>
         <Services/>
         <Portfolio/>
-        <Testimonials/>
         <Contact/>
         <Footer/>
       </div>
